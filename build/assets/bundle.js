fb0db364006d96cf9cfe7d38a1d4e2c9cbb132f6
Yuriy Bevov <jury19y@mail.ru>
2022-04-19 19:01:40 +0300

    project-page

---
+++ build/assets/bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*****************************!*\
+  !*** ./src/scripts/main.js ***!
+  \*****************************/
+//import './modules/module.js';
+//import "./vue/main.js";
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
