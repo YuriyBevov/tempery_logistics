516218cabff5a6f4293315a28685fd4b70d1efe9
Yuriy Bevov <jury19y@mail.ru>
2022-05-14 20:16:22 +0300

    search-field

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,48 +4,5 @@ import './modules/accordionLineAnimation.js';
 import './modules/pageOffset.js';
 import './modules/header.js';
 import './modules/carousel.js';
-//import './modules/searchField.js';
+import './modules/searchField.js';
 import './modules/sectionAnimation.js';
-
-import gsap from 'gsap';
-
-const searchOpener = document.querySelector('.search-opener');
-const searchField = document.querySelector('.search-form');
-const navItems = document.querySelectorAll('.main-nav .nav-item');
-const searchCloser = document.querySelector('.search-closer');
-
-const onClickCloseSearchField = () => {
-  gsap.to(searchField, {duration: 0.5, y: -300, delay: 0.2, ease: 'ease-in'});
-
-  gsap.to(searchOpener, {duration: 0.7, scale: 1, delay: 0.2, ease: 'ease-in'});
-  navItems.forEach((item,i) => {
-    gsap.to(item, {
-      duration: 0.7,
-      scale: 1,
-      delay: 0.2,
-      ease: "ease-in"
-    });
-  })
-  searchCloser.removeEventListener('click', onClickCloseSearchField);
-  searchOpener.addEventListener('click', onClickOpenSearchField);
-}
-
-const onClickOpenSearchField = () => {
-
-  gsap.to(searchOpener, {duration: 0.5, scale: 0, delay: 0.2, ease: 'ease-in'});
-  navItems.forEach((item,i) => {
-    gsap.to(item, {
-      duration: 0.5,
-      scale: 0,
-      delay: 0.2 + (0.05 * i),
-      ease: "ease-in"
-    });
-  })
-
-  gsap.to(searchField, {duration: 0.5, y: 0, delay: 0.8, ease: 'ease-in'});
-
-  searchOpener.removeEventListener('click', onClickOpenSearchField);
-  searchCloser.addEventListener('click', onClickCloseSearchField);
-}
-
-searchOpener.addEventListener('click', onClickOpenSearchField);
--- src/scripts/modules/searchField.js
+++ src/scripts/modules/searchField.js
@@ -1,20 +1,42 @@
-const searchForm = document.querySelector('.search-field form');
+import gsap from 'gsap';
 
-if(searchForm) {
-  let searchBtn = document.querySelector('.search-field button[type="submit"');
-  let searchField = document.querySelector('.search-field input[type="search"');
+const searchOpener = document.querySelector('.search-opener');
+const searchField = document.querySelector('.search-form');
+const navItems = document.querySelectorAll('.main-nav .nav-item');
+const searchCloser = document.querySelector('.search-closer');
 
+const onClickCloseSearchField = () => {
+  gsap.to(searchField, {duration: 1.2, y: -300, ease: 'ease-in'});
 
-  const onClickShowSearchField = (evt) => {
-    evt.preventDefault();
-    if(!searchField.classList.contains('js-active')) {
-      searchField.classList.add('js-active');
-    } else {
-      searchField.classList.remove('js-active');
-      searchField.value = '';
-      console.log(searchForm, 'submit')
-    }
-  }
-
-  searchBtn.addEventListener('click', onClickShowSearchField);
+  gsap.to(searchOpener, {duration: 0.7, scale: 1, delay: 0.2, ease: 'ease-in'});
+  navItems.forEach((item,i) => {
+    gsap.to(item, {
+      duration: 0.7,
+      scale: 1,
+      delay: 0.2,
+      ease: "ease-in"
+    });
+  })
+  searchCloser.removeEventListener('click', onClickCloseSearchField);
+  searchOpener.addEventListener('click', onClickOpenSearchField);
 }
+
+const onClickOpenSearchField = () => {
+
+  gsap.to(searchOpener, {duration: 0.5, scale: 0, delay: 0.2, ease: 'ease-in'});
+  navItems.forEach((item,i) => {
+    gsap.to(item, {
+      duration: 0.5,
+      scale: 0,
+      delay: 0.2 + (0.05 * i),
+      ease: "ease-in"
+    });
+  })
+
+  gsap.to(searchField, {duration: 0.5, y: 0, delay: 0.8, ease: 'ease-in'});
+
+  searchOpener.removeEventListener('click', onClickOpenSearchField);
+  searchCloser.addEventListener('click', onClickCloseSearchField);
+}
+
+searchOpener.addEventListener('click', onClickOpenSearchField);
