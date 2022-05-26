79a891b9137305f8567ec64ada1a46d7b04e0a20
Yuriy Bevov <jury19y@mail.ru>
2022-05-26 11:17:04 +0300

    vh

--- build/assets/bundle.js
+++ build/assets/bundle.js
@@ -19132,7 +19132,18 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
+ // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 
+var vh = window.innerHeight * 0.01; // Then we set the value in the --vh custom property to the root of the document
+
+document.documentElement.style.setProperty('--vh', "".concat(vh, "px")); // We listen to the resize event
+
+window.addEventListener('resize', function () {
+  // We execute the same script as before
+  var vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', "".concat(vh, "px"));
+  console.log(vh, 'resize');
+});
 })();
 
 /******/ })()
